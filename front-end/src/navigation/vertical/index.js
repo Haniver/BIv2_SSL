2256d8aea26bdcb9161bd3f35ecb57da39563d5c
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2022-04-26 09:06:09 -0500

    redirigir a logout cuando los datos locales difieren de la estructura del menú en la api

--- front-end/src/navigation/vertical/index.js
+++ front-end/src/navigation/vertical/index.js
@@ -1,3 +1,5 @@
+import AuthService from "@src/services/auth.service"
+
 import { Truck, Home, BarChart2, Search, Clock, DollarSign, FileMinus, Award, Monitor, Grid, MapPin, Watch, Sunrise, Volume2, CornerLeftDown, Box, ShoppingBag, Rewind, RotateCw, Tool, UserPlus, Map, HelpCircle, Database, Code, FileText } from 'react-feather'
 
 const dashboards = []
@@ -18,11 +20,12 @@ const navigation = [
 ]
   const userData = JSON.parse(localStorage.getItem('userData'))
   if (userData !== null) {
-    // console.log(userData["vistas"])
     const vistas = JSON.parse(userData["vistas"])
     const documentos_api = JSON.parse(userData["documentos"])
     // console.log('vistas:')
     // console.log(vistas)
+    // console.log('documentos_api:')
+    // console.log(documentos_api)
     if (vistas !== undefined) {
       const iconos = {
         Truck, 
@@ -54,36 +57,44 @@ const navigation = [
       }
 
       vistas.forEach(elemento => {
-        const Icono = iconos[elemento.icon]
-        const objeto_a_insertar = {
-          id: elemento.idReact,
-          id_vista: elemento.id_vista,
-          title: elemento.title,
-          icon: <Icono size={30} />,
-          navLink: `/${elemento.idReact}`
-        }
-        if (elemento.categoria === 'Dashboard') {
-          dashboards.push(objeto_a_insertar)
-        } else if (elemento.categoria === 'Reporte') {
-          reportes.push(objeto_a_insertar)
-        } else if (elemento.categoria === 'Herramientas') {
-          herramientas.push(objeto_a_insertar)
+        if (elemento.hasOwnProperty('icon') && elemento.hasOwnProperty('idReact') && elemento.hasOwnProperty('id_vista') && elemento.hasOwnProperty('title') && elemento.hasOwnProperty('categoria')) {
+          const Icono = iconos[elemento.icon]
+          const objeto_a_insertar = {
+            id: elemento.idReact,
+            id_vista: elemento.id_vista,
+            title: elemento.title,
+            icon: <Icono size={30} />,
+            navLink: `/${elemento.idReact}`
+          }
+          if (elemento.categoria === 'Dashboard') {
+            dashboards.push(objeto_a_insertar)
+          } else if (elemento.categoria === 'Reporte') {
+            reportes.push(objeto_a_insertar)
+          } else if (elemento.categoria === 'Herramientas') {
+            herramientas.push(objeto_a_insertar)
+          }
+        } else {
+          AuthService.logout()
         }
       })
       documentos_api.forEach(elemento => {
-        const Icono = iconos[elemento.icon]
-        const objeto_a_insertar = {
-          id: elemento.nombre_archivo,
-          title: elemento.title,
-          icon: <Icono size={30} />,
-          navLink: `/documentos/${elemento.nombre_archivo}`
-        }
-        if (elemento.categoria === 'Tutorial') {
-          docsTutorial.push(objeto_a_insertar)
-        } else if (elemento.categoria === 'Codigo') {
-          docsCodigo.push(objeto_a_insertar)
-        } else if (elemento.categoria === 'BD') {
-          docsBD.push(objeto_a_insertar)
+        if (elemento.hasOwnProperty('icon') && elemento.hasOwnProperty('nombre_archivo') && elemento.hasOwnProperty('id_vista') && elemento.hasOwnProperty('title') && elemento.hasOwnProperty('categoria')) {
+          const Icono = iconos[elemento.icon]
+          const objeto_a_insertar = {
+            id: elemento.nombre_archivo,
+            title: elemento.title,
+            icon: <Icono size={30} />,
+            navLink: `/documentos/${elemento.nombre_archivo}`
+          }
+          if (elemento.categoria === 'Tutorial') {
+            docsTutorial.push(objeto_a_insertar)
+          } else if (elemento.categoria === 'Codigo') {
+            docsCodigo.push(objeto_a_insertar)
+          } else if (elemento.categoria === 'BD') {
+            docsBD.push(objeto_a_insertar)
+          }
+        } else {
+          AuthService.logout()
         }
       })
       if (reportes.length > 0) {
@@ -152,6 +163,8 @@ const navigation = [
           }
         )
       }
+    } else {
+      AuthService.logout()
     }
   }
 
