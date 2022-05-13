cd78a68e51e2b2352a58ba15f5b89583fb5801f3
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2022-05-13 08:26:26 -0500

    Agregar una forma más de hacer logout si el jwt no es correcto

--- front-end/src/services/auth.header.js
+++ front-end/src/services/auth.header.js
@@ -1,10 +1,15 @@
 import Cookies from 'js-cookie'
+import authService from './auth.service'
 
 export default function authHeader() {
   // const user = JSON.parse(localStorage.getItem('user')); // No vas a usar esto porque no necesitas obtener toda la información del usuario del almacenamiento local para saber el token; ese lo sacas de las cookies
 
   let jwt = Cookies.get('tokenHeaderPayload') + Cookies.get('tokenSignature')
-  jwt = jwt.slice(1, -1)
+  if (jwt.length > 0) {
+    jwt = jwt.slice(1, -1)
+  } else {
+    authService.logout
+  }
   // if (user && user.accessToken) { // Lo mismo que arriba
   if (jwt) {
     return { Authorization: `Bearer ${jwt}` } // for Spring Boot back-end
