29430e1c6d0d129f41ab242ce3ad4e761ce74c2a
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2022-05-19 08:39:57 -0500

    Tabla Pedidos no entregados o no cancelados de dashboard PedidosPendientes tiene opciones de paginación de 20 en 20 hasta 100

--- front-end/src/views/PedidosPendientes.js
+++ front-end/src/views/PedidosPendientes.js
@@ -40,7 +40,7 @@ const PedidosPendientes = () => {
       </Row>
       <Row className='match-height'>
       {(tienda === '' || tienda === undefined || tienda === false) && <Col sm='12'>
-          <Tabla titulo='Pedidos No Entregados o No Cancelados' seccion={seccion} region={region} zona={zona} tienda={tienda} tipoEntrega={tipoEntrega} opcionesPaginacion={[5, 10, 20, 50, 100]} />
+          <Tabla titulo='Pedidos No Entregados o No Cancelados' seccion={seccion} region={region} zona={zona} tienda={tienda} tipoEntrega={tipoEntrega} opcionesPaginacion={[20, 40, 60, 80, 100]} />
         </Col>}
       {(tienda !== '' && tienda !== undefined && tienda !== false) && <Col sm='12'>
           <Tabla titulo={`Pedidos No Entregados o No Cancelados ${tienda}`} tituloAPI='Pedidos No Entregados o No Cancelados $tienda' seccion={seccion} region={region} zona={zona} tienda={tienda} tipoEntrega={tipoEntrega} opcionesPaginacion={[5, 10, 20, 50, 100]} />
