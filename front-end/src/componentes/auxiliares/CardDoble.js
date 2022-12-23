a9f48829dd5a0be7fab09a1707c7776fa2903075
GUTIERREZ MUNCH <lgutierrezm@gcch.com>
2022-12-23 12:18:16 -0600

    Quité el fondo blanco (o sea, los estilos de Card) a encabezado

---
+++ front-end/src/componentes/auxiliares/CardDoble.js
@@ -0,0 +1,23 @@
+// ** Third Party Components
+// import PropTypes from 'prop-types'
+import { Card, CardBody } from 'reactstrap'
+import authHeader from '../../services/auth.header'
+import axios from 'axios'
+import { useState, useEffect, useReducer } from 'react'
+import CustomUrls from '../../services/customUrls'
+import LoadingGif from '../auxiliares/LoadingGif'
+import fechas_srv from '../../services/fechas_srv'
+
+const Titulo = ({titulo, subtitulo}) => {
+  const [hayError, setHayError] = useState(false)
+
+  return (
+    <>
+        <h3 className='titulo'>{titulo.toUpperCase()}</h3>
+        <p className='subtitulo'>{subtitulo}</p>
+        <hr className='hrTitulo' />
+    </>
+  )
+}
+
+export default Titulo
