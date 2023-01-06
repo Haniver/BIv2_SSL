68972de439ec09d9ca6c2dd932a85b01f9c83589
GUTIERREZ MUNCH <lgutierrezm@gcch.com>
2023-01-06 16:05:35 -0600

    Hice una modificación al combo de anio del filtro en el commit pasado donde el strict mode quebraba. Ya se arregló.

--- front-end/src/componentes/auxiliares/Filtro.js
+++ front-end/src/componentes/auxiliares/Filtro.js
@@ -187,16 +187,14 @@ const Filtro = (props) => {
 
   const comboAnio = []
   const comboAnioOpcional = []
-  const comboAnioRFM = [
-    {label: `${fechas_srv.anioActual()}`, value: fechas_srv.anioActual()}
-  ]
+  const comboAnioRFM = [{label: `${fechas_srv.anioActual()}`, value: fechas_srv.anioActual()}]
   const meterMasAnios = Math.abs(2022 - fechas_srv.anioActual())
   for (let i = 0; i <= meterMasAnios; i++) {
     comboAnio.push({label: `${fechas_srv.anioActual() - i}`, value: fechas_srv.anioActual() - i})
     comboAnioOpcional.push({label: `${fechas_srv.anioActual() - 1}`, value: fechas_srv.anioActual() - 1})
     comboAnioRFM.push({label: `${fechas_srv.anioActual() - i - 1}`, value: fechas_srv.anioActual() - i - 1})
   }
-  
+
   const comboMes = []
   for (let i = 0; i < 12; i++) {
     comboMes.push({label: `${fechas_srv.mesTexto(i, true)}`, value: i})
